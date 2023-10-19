ee643e8918daae2530b38681631bba71819f4d47
JayPalkar <jaypalkar654@gmail.com>
2023-10-19 09:35:52 +0530

    OTP assignment version 2 with functions and Regular expresion

--- OTP.py
+++ OTP.py
@@ -1,18 +1,47 @@
 import random
+import re
 import smtplib as smtp
 
 
+# fintion to generate an OTP
+def generateotp():
+    otp = str(random.randint(100000, 999999))
+    return otp
+
+
+# funtion to take mail as input form user
+def inputmail():
+    email = input("Please Enter your Email: ")
+    return email
+
+
+# function to validate the email
+def validatemail(mail):
+    validation_condition = "^[a-z]+[\._]?[a-z 0-9]+[@]\w+[.]\w{2,3}$"  # validation using regular expression
+    if re.search(validation_condition, mail):
+        return True
+    else:
+        return False
+
+
+# function to send mail
 def sendmail(to, OTP):
     server = smtp.SMTP('smtp.gmail.com', 587)
     server.starttls()
     server.login('jaywebsitetest@gmail.com', 'jmjobvzzjbhzxbxz')
-    server.sendmail('jaywebsitetest@gmail.com', to, OTP)
-    print("OTP send")
+    str = f"""your Otp for login is {OTP}.
+    Please don't share this OTP with anyone.
+    Thank you."""
+    server.sendmail('jaywebsitetest@gmail.com', to, str)
+    print("Success✅🎉! OTP send successfully")
 
 
+# main funtion
 if __name__ == '__main__':
-    email = input("Please Enter your Email: ")
-    otp = str(random.randint(100000, 999999))
-
-    # print(otp)
-    sendmail(email, otp)
+    otp = generateotp()  # calling generateotp funtion
+    email = inputmail()  # calling inputmail function
+    validation = validatemail(email)  # calling validatemail function
+    if validation:  # if email is correct then send the mail
+        sendmail(email, otp)
+    else:
+        print("You entered wrong email please❌ try again with proper email")
